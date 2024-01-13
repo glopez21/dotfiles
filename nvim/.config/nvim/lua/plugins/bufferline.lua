30adaa384f182e541c58259d9aeb5b92abe1999d
glopez21 <genghis.lopez@gmail.com>
2024-01-13 13:27:35 -0500

    new plugins --> options, autopairs, debug, lightbulb..etc

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
         'akinsho/bufferline.nvim', version = "*",
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
+            vim.opt.termguicolors = true
             local builtin = require("bufferline").setup{}
         end
     }
